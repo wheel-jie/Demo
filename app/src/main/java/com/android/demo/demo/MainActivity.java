61888bcb187223bd8096e35c5634b2912d8f8491
夜末的寂寞 <576298282@qq.com>
2019-04-21 22:27:15 +0800

    Update MainActivity.java

--- app/src/main/java/com/android/demo/demo/MainActivity.java
+++ app/src/main/java/com/android/demo/demo/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //我是提交
     }
 }
