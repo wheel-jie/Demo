fb37f6f94ee259dd1a89ce5d7a990827555776d9
wangjie <wangjiesmile0228@gmail.com>
2019-04-21 22:29:41 +0800

    啦啦

--- app/src/main/java/com/android/demo/demo/MainActivity.java
+++ app/src/main/java/com/android/demo/demo/MainActivity.java
@@ -7,7 +7,6 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 
-    //我是b
     private TextView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
